435b89fa57d410f589d71d50aa0f45e0d5ca968d
StarDrop9 <kurtstarck@yahoo.com>
2019-12-22 10:49:37 -0600

    toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './assets/tm.jpg';
 import './App.css';
-
+import Toolbar from './toolbar'
 const App: React.FC = () => {
   return (
+    
+    
+
     <div className="App">
       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <h2 >React App with JWT Authentication with Sqllite3 and play chess & blackjack & show U tube favorites with Todos</h2> 
-               </p>
-       </header>
+      <Toolbar />
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2 >React Chat App with JWT Authentication with Sqllite3 and play chess & blackjack & show U tube favorites with Todos</h2> 
+      </header>
     </div>
   );
 }
