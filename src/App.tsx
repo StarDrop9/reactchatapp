a00cb40a455cecbc741c81a7228471406cb19ab9
StarDrop9 <kurtstarck@yahoo.com>
2019-12-22 09:32:51 -0600

    1st App.tsx Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/tm.jpg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2 >React App with JWT Authentication with Sqllite3 and play chess & blackjack & show U tube favorites with Todos</h2> 
+               </p>
+       </header>
     </div>
   );
 }
